2c130c54b9975857ba6c1189195937d33fab995c
rbpolim <rbpolim@gmail.com>
2024-01-03 15:15:01 -0300

    feat: config auth jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
-import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
+
+import { PrismaService } from './prisma/prisma.service'
+
+import { CreateAccountController } from './controllers/create-account.controller'
+
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -11,6 +15,7 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
