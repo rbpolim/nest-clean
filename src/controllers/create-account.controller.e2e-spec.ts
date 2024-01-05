3a810f323aca65b8660172c655d99442d072524f
rbpolim <rbpolim@gmail.com>
2024-01-05 14:23:58 -0300

    test: created-account e2e test

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,39 @@
-test('Sum', () => {
-  expect(1 + 1).toBe(2)
+import request from 'supertest'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+
+describe('Create account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: { email: 'johndoe@email.com' },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
 })
